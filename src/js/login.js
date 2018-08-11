b2c1585ec78a2c9f01f5c55f0a7c2786c0c5a8ab
Josce Jaimes <nanayhachi94@gmail>
2018-08-11 15:06:28 -0500

    Eliminando codigo muerto

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,6 @@
 // JS p/ login.html//
 // alert('Ya está enlazado');
+
 var config = {
   apiKey: 'AIzaSyBBL2vG5GR2GZnUkLrpPZ-mDbyKie8nxgM',
   authDomain: 'registro-de-visitantes-a0747.firebaseapp.com',
@@ -8,13 +9,15 @@ var config = {
   storageBucket: 'registro-de-visitantes-a0747.appspot.com',
   messagingSenderId: '629215938937'
 };
-// firebase.initializeApp(config);
+firebase.initializeApp(config);
+
 function registro() {
   console.log('diste un click');
   let nombre = document.getElementById('nombre').value;
   let email = document.getElementById('correo').value;
   let contraseña = document.getElementById('contraseña').value;
   console.log(nombre, email, contraseña);
+
   firebase.auth().createUserWithEmailAndPassword(email, contraseña).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -39,24 +42,24 @@ function entrar() {
   });
 }
 
-let btnAddMember = document.getElementById('newMember')
-let names = document.getElementById('name');
-let email = document.getElementById('mail');
-let password = document.getElementById('ps');
+// let btnAddMember = document.getElementById('newMember')
+// let names = document.getElementById('name');
+// let email = document.getElementById('mail');
+// let password = document.getElementById('ps');
 
 
-btnAddMember.addEventListener('click', (e) => {  
-  console.log(e);
-  let nm = names.value;
-  console.log(nm);
+// btnAddMember.addEventListener('click', (e) => {  
+//   console.log(e);
+//   let nm = names.value;
+//   console.log(nm);
   
-  let pass = password.value;
-  console.log(pass);
+//   let pass = password.value;
+//   console.log(pass);
   
-  let ml = email.value;
-  console.log(ml);
+//   let ml = email.value;
+//   console.log(ml);
   
-  const auth = firebase.auth();
-  const promise = auth.createUserWithEmailAndPassword(ml, pass);
-});
-
+//   const auth = firebase.auth();
+//   const promise =
+ auth.createUserWithEmailAndPassword(ml, pass);
+// });
