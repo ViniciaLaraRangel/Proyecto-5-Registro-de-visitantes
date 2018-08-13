260f1e70eec6152dd499584d16504ca0a6a7e89a
Leticia Vianey Lara Rangel <vianey.lara.rangel@hotmail.com>
2018-08-13 16:32:16 -0500

    Cámara cuadra en la caja correcta

--- src/js/photo-shot.js
+++ src/js/photo-shot.js
@@ -1,20 +1,18 @@
-
-
- console.log("HOLA!");	
- (function(){
- var video = document.getElementById('video'),
- vendorUrl = window.URL || window.webkitURL;
- navigator.getMedia = (navigator.getUserMedia ||
+console.log('HOLA!');	
+(function() {
+  var video = document.getElementById('video'),
+    vendorUrl = window.URL || window.webkitURL;
+  navigator.getMedia = (navigator.getUserMedia ||
  navigator.webkitGetUserMedia ||
  navigator.mozGetUserMedia ||
  navigator.msGetUserMedia);
- navigator.getMedia ({
- video: true,
- audio: false
- }, function(stream){
- video.src = window.URL.createObjectURL(stream);
- video.play();
- }, function (error){
- console.log("error!");
- }); 
- }) ();
+  navigator.getMedia({
+    video: true,
+    audio: false
+  }, function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+  }, function(error) {
+    console.log('error!');
+  }); 
+})();
