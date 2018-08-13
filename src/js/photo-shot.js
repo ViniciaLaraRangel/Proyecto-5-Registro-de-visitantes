7d83a2b94dfddb3b762f743efe51a54ebf096a88
Leticia Vianey Lara Rangel <vianey.lara.rangel@hotmail.com>
2018-08-13 11:36:59 -0500

    Mejorando caja de <canvas> y <video>

--- src/js/photo-shot.js
+++ src/js/photo-shot.js
@@ -1,18 +1,20 @@
-// console.log('Si enlaza');	
-(function() {
-  var video = document.getElementById('video'),
-    vendorUrl = window.URL || window.webkitURL;
-  navigator.getMedia = (navigator.getUserMedia ||
-   navigator.webkitGetUserMedia ||
-   navigator.mozGetUserMedia ||
-   navigator.msGetUserMedia);
-  navigator.getMedia({
-    video: true,
-    audio: false
-  }, function(stream) {
-    video.src = window.URL.createObjectURL(stream);
-    video.play();
-  }, function(error) {
-    console.log('error!');
-  }); 
-})();
+
+
+ console.log("HOLA!");	
+ (function(){
+ var video = document.getElementById('video'),
+ vendorUrl = window.URL || window.webkitURL;
+ navigator.getMedia = (navigator.getUserMedia ||
+ navigator.webkitGetUserMedia ||
+ navigator.mozGetUserMedia ||
+ navigator.msGetUserMedia);
+ navigator.getMedia ({
+ video: true,
+ audio: false
+ }, function(stream){
+ video.src = window.URL.createObjectURL(stream);
+ video.play();
+ }, function (error){
+ console.log("error!");
+ }); 
+ }) ();
