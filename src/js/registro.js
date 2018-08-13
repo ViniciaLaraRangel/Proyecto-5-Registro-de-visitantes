0f4cbfa39510bbf7c957703d71ab12d45e6c9e16
Josce Jaimes <nanayhachi94@gmail>
2018-08-13 11:31:43 -0500

    Corrigiendo login y funcionalidad

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,14 +1,14 @@
 // JS p/ registro.html//
 // Initialize Firebase
-var config = {
-  apiKey: 'AIzaSyBBL2vG5GR2GZnUkLrpPZ-mDbyKie8nxgM',
-  authDomain: 'registro-de-visitantes-a0747.firebaseapp.com',
-  databaseURL: 'https://registro-de-visitantes-a0747.firebaseio.com',
-  projectId: 'registro-de-visitantes-a0747',
-  storageBucket: 'registro-de-visitantes-a0747.appspot.com',
-  messagingSenderId: '629215938937'
-};
-firebase.initializeApp(config);
+// var config = {
+//   apiKey: 'AIzaSyBBL2vG5GR2GZnUkLrpPZ-mDbyKie8nxgM',
+//   authDomain: 'registro-de-visitantes-a0747.firebaseapp.com',
+//   databaseURL: 'https://registro-de-visitantes-a0747.firebaseio.com',
+//   projectId: 'registro-de-visitantes-a0747',
+//   storageBucket: 'registro-de-visitantes-a0747.appspot.com',
+//   messagingSenderId: '629215938937'
+// };
+// firebase.initializeApp(config);
 
 var db = firebase.firestore();
 
